afebc37bb4102a8853ea16831b3e8c238021f026
Joe Lynn <josephlynn94@gmail.com>
2020-09-25 23:58:08 -0400

    finished validating and updating values with JS

---
+++ index.js
@@ -0,0 +1,70 @@
+const variation1 = document.querySelector("#variation1")
+const variation2 = document.querySelector("#variation2")
+const variation3 = document.querySelector("#variation3")
+const variation4 = document.querySelector("#variation4")
+const variation5 = document.querySelector("#variation5")
+const variation6 = document.querySelector("#variation6")
+const variation7 = document.querySelector("#variation7")
+
+const name = document.querySelector("#yourName");
+
+function getNumberOfChars(name) {
+  // return the number of characters in: name
+  return name.length
+}
+
+function getFirstChar(name) {
+  // return the first character of: name
+  return name[0]
+}
+
+function getLastChar(name) {
+  // return the last character of: name
+  return name[name.length - 1]
+}
+
+function getLowerChar(name) {
+  // return name all in lower case (example: "ABC" becomes "abc")
+  return name.toLowerCase();
+}
+
+function getUpperChar(name) {
+  // return name all in upper case (example: "abc" becomes "ABC")
+  return name.toUpperCase();
+}
+
+function getCapitalized(name) {
+  // return a capitalized version of name (example: "alEX" becomes "Alex")
+  return  name[0].toUpperCase() + name.substring(1).toLowerCase()
+}
+
+function getTrim(name) {
+  // return name without trailing and leading space (example: " alex " becomes: "alex")
+  return name.trim()
+}
+
+// listen for key events 
+name.addEventListener("keyup", () => {
+  // get the value of the input 
+  const value = name.value;
+
+  if(value == "") {
+    variation1.textContent = ""
+    variation2.textContent = ""
+    variation3.textContent = ""
+    variation4.textContent = ""
+    variation5.textContent = ""
+    variation6.textContent = ""
+    variation7.textContent = ""
+  } else {
+    variation1.textContent = getNumberOfChars(value);
+    variation2.textContent = getFirstChar(value);
+    variation3.textContent = getLastChar(value);
+    variation4.textContent = getLowerChar(value);
+    variation5.textContent = getUpperChar(value);
+    variation6.textContent = getCapitalized(value);
+    variation7.textContent = getTrim(value);
+  }
+
+  console.log(value)
+})
